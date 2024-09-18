a93143a045460bcf72d7daecdffe58f65659b7b4
s Shashi <shashixcello@gmail.com>
2024-09-18 20:26:18 +0530

    Initial commit

---
+++ app/dashboard/_components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+import { UserButton } from '@clerk/nextjs'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between items-center p-5 shadow-md'>
+       
+       <div className="flex gap-2"> <Image src={'/ai.png'} width={30} height={20} className=''/>
+        <h2 className='mt-1 font-bold text-1xl'>Magna-AI</h2>
+        </div>
+        <UserButton />
+    </div>
+    
+  )
+}
+
+export default Header
---
+++ app/dashboard/_components/SideBar.jsx
@@ -0,0 +1,68 @@
+'use client'
+import React, { useContext } from 'react'
+import Image from 'next/image'
+import { HomeIcon,Layers,ShieldPlus,LogOut } from 'lucide-react'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+import { Progress } from '@/components/ui/progress'
+import { UserCourseListContext } from '@/app/_context/UserCourseList'
+
+
+
+function SideBar() {
+
+  const {userCourseList,setUserCourseList} = useContext(UserCourseListContext)
+  const Menu = [
+
+    {
+      id: 1,
+      name: 'Home',
+      icon: HomeIcon,
+      path: '/dashboard'
+    },
+    {
+      id: 1,
+      name: 'Explore',
+      icon: Layers,
+      path: '/dashboard/explore'
+    },
+    {
+      id: 1,
+      name: 'Upgrade',
+      icon: ShieldPlus,
+      path: '/dashboard/upgrade'
+    },
+    {
+      id: 1,
+      name: 'LogOut',
+      icon: LogOut,
+      path: '/dashboard/logout'
+    }
+   ]
+   const path =usePathname();
+
+  return (
+    
+    <div className='fixed  h-full md:w-64 p-5 shadow-md'>
+      <Image src={'/ai.png'} width={30} height={10} alt='logo'/>
+      <hr className="my-5" />
+      <ul>
+        {Menu.map((item,index)=>(
+          <Link href={item.path}>
+          <div className={`flex items-center gap-2 text-gray-600 p-3 cursor-pointer hover:bg-gray-300 te rounded-lg mb-2 
+          ${item.path==path && 'bg-gray-300 text-black'}`}>
+            <h2 className='text-center ml-15'>{item.name}</h2>
+          </div>
+          </Link>
+        ))}
+      </ul>
+      <div className="absolute bottom-10 w-[80%] ">
+        <Progress value={(userCourseList?.length/5)*100} className="bg-gradient-to-r from-green-100 to-sky-400 text-white"/>
+        <h2 className='text-sm my-2'>{userCourseList?.length} out 5 course is created</h2>
+        <h2 className='text-xs text-gray-500'>Upgrade your plan for unLimited course generation</h2>
+      </div>
+    </div>
+  )
+}
+
+export default SideBar
